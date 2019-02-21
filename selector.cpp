d81e548a02ce7887e93ea319974506f7b1564199
Mario <marioyales2@gmail.com>
2019-02-22 00:30:09 +0100

    still missing the constructor

--- selector.cpp
+++ selector.cpp
@@ -3,6 +3,16 @@
 #include <iostream>
 #include <vector>
 #include "TH1.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TLorentzVector.h"
+
+// Selector corre una vez por muestra.
+
+// TODO: pintar todos los MC, uno encima del otro
+// TODO: pintar los datos en el mismo histo
+// TODO: crear histograma cociente
+// TODO: aplicar cortes
 
 
 class Selector
@@ -10,7 +20,9 @@ class Selector
 
   public:
     
-    TString prefix; //prefix for ALL histos
+    TString prefix;                 // prefix for ALL histos
+    TString fileName;               // name of the root file
+    TString filePath;               // path of the root file
     std::vector<TH1F*> histograms;  
     
     void CreateHistograms()
@@ -29,15 +41,66 @@ class Selector
         int len = histograms.size();
         for (int i = 0; i < len; i++)
         {
-            TString n = histograms[i]->GetName(); //full name
+          TString n = histograms[i]->GetName(); //full name
             
-            if ((prefix + '_' + name) == n)
-                return *histograms[i];
+          if ((prefix + '_' + name) == n)
+            return *histograms[i];
         }
 
         std::cout << "[Selector::GetHisto] WARNING: not found histo named " + \
         name + ". Returning an empty one" << std::endl;
         return TH1F();
+    }
+
+    void Loop()
+    {
+      TFile f(filePath + fileName);
+      TTree * tree = (TTree *) f.Get("events");
+
+      Int_t numEvents = tree->GetEntries();
+
+      // Set Adresses to branches >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      Int_t NMuon;
+      Bool_t triggerIsoMu24;
+      Float_t Muon_Px[3]; // just in case... memory is not a problem here
+      Float_t Muon_Py[3];
+      Float_t Muon_Pz[3];
+      Float_t Muon_E[3];
+      Float_t EventWeight;
+
+      tree->SetBranchAddress("NMuon", &NMuon);
+      tree->SetBranchAddress("Muon_Px", &Muon_Px);
+      tree->SetBranchAddress("Muon_Py", &Muon_Py);
+      tree->SetBranchAddress("Muon_Pz", &Muon_Pz);
+      tree->SetBranchAddress("Muon_E", &Muon_E);
+      tree->SetBranchAddress("triggerIsoMu24", &triggerIsoMu24);
+      tree->SetBranchAddress("EventWeight", &EventWeight);
+      // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+      // Loop over every event >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      for (int i = 0; i < numEvents; i++)
+      {
+        tree->GetEntry(i);
+
+        if (!triggerIsoMu24) {continue;}
+        if (NMuon != 1) {continue;} 
+
+        TLorentzVector muon;
+        muon.SetPxPyPzE(Muon_Px[0], Muon_Py[0], Muon_Pz[0], Muon_E[0]);
+        
+        Float_t weight;
+        if (fileName != "data") {weight = EventWeight;} else weight = 1;
+
+        // full analysis >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  
+
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        // fill histos >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        GetHisto('MuonPt').Fill(muon.Pt()); //C'mon Mario, you can do it better 
+
+        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+      }
     }
 
 };
@@ -47,6 +110,9 @@ void selector()
 {
     Selector obj;
     obj.prefix = "hello";
+    obj.filePath = "./practica/files/";
+    obj.fileName = "dy.root";
     obj.CreateHistograms();
+    obj.Loop();
     obj.GetHisto("MuonPst");
 }
