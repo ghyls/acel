3a5963a89997699272ed2227ab01db10e76836f6
Mario <marioyales2@gmail.com>
2019-02-24 20:16:21 +0100

    fucking changed everything

--- plotter.cpp
+++ plotter.cpp
@@ -1,145 +1,190 @@
 
 #include "selector.h"
+#include "plotter.h"
 
 #include <iostream>
 #include <vector>
 #include <string>
 #include "TH1.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "THStack.h"
 
 
-class Plotter
+Float_t fLegX1 = 0.70, fLegY1 = 0.55, fLegX2 = 0.92, fLegY2 = 0.92;
+Float_t LegendTextSize  = 0.065;
+TString xtitle = "";
+TString ytitle = "";
+TString title  = "";    
+Selector* dataSelector;
+
+
+Plotter::Plotter(std::vector<std::string> bkgs, std::string pathToFiles,  
+std::string prefixForHistos, std::string data = "")
 {
+  data = data;
+  listOfSelectors = {}; // initialization!
 
-  public:
-
-    Float_t fLegX1 = 0.70, fLegY1 = 0.55, fLegX2 = 0.92, fLegY2 = 0.92;
-    Float_t LegendTextSize  = 0.065;
-    TString xtitle = "";
-    TString ytitle = "";
-    TString title  = "";    
-    Selector* dataSelector;
-
-    Plotter(std::vector<std::string> bkgs, std::string pathToFiles,  
-    std::string prefixForHistos, std::string data = "")
-    {
-      data = data;
-
-      for (int i = 0; i < bkgs.size(); i++)
-      {
-        Selector sel(pathToFiles, bkgs[i], prefixForHistos);
-
-        listOfSelectors.push_back(sel); 
-        listOfColors.push_back(i+1);
-      }
-
-      if (data != "")
-        dataSelector = new Selector(pathToFiles, data, prefixForHistos);
-    }
-
-    void SetLegendPos(Float_t x1, Float_t y1, Float_t x2, Float_t y2)
-    {
-      fLegX1 = x1; 
-      fLegX2 = x2; 
-      fLegY1 = y1; 
-      fLegY2 = y2; 
-    }
-    
-    void SetLegendSize(Float_t size = 0.065)
-    {
-      LegendTextSize = size;
-    }
-
-    void SetColors(std::vector<Int_t> col)
-    {
-      listOfColors = col;
-    }
-
-    void SetTitle(TString _title)
-    {
-      title = _title;
-    }
-    
-    void SetXTitle(TString _xtitle)
-    {
-      xtitle = _xtitle;
-    }
-    void SetYTitle(TString _ytitle)
-    {
-      ytitle = _ytitle;
-    }
-
-    TH1F* GetHisto(TString name) //name is the name of the histo
-    {
-        //TODO: I don't work for a specific process!
-        //TODO: Number of histos could depend on the process (?)
-        //FIXME: Algo me dice que esto solo devuelve la primera coincidencia.
-        
-        int numberOfSelectors = listOfSelectors.size();
-        int numberOfHistos = listOfSelectors[0].GetNumberOfHistos();
-
-        for (int i = 0; i < numberOfSelectors; i++)
-        {
-          for (int j = 0; i < numberOfHistos; i++)
-          {
-            TString n = listOfSelectors[i].histograms[j]->GetName(); //full name
-
-            if (!std::string(n).find(std::string(name))){continue;}
-            //si no encuentras el histo, sigue adelante
-
-            return listOfSelectors[i].histograms[j];
-          }
-        }
-        throw;
-    }
-
-    int GetEvents(TString name)
-    {
-      // Returns the integral of a histogram
-      return GetHisto(name)->Integral();
-    }
-
-    void PrintEvents(TString name)
-    {
-      // Name is a name. Not a list or any other weird thing.
-
-      std::cout << "\nPrinting number of events for histogram " << name << "\n";
-      std::cout << "---------------------------------------------" << std::endl;
-
-      Int_t totalEvts = 0;
-
-      for (int i = 0; i < listOfSelectors.size(); i++)
-      {
-        TH1F* h = listOfSelectors[i].GetHisto(name);
-
-        std::cout << name << ": " << h->Integral();
-        totalEvts += h->Integral();
-      }
-
-      std::cout << "Total expected: " << totalEvts << std::endl;
-      std::cout << "--------------------" << std::endl;
-
-      if (data != "")
-      {
-        TH1F* h = dataSelector->GetHisto(name);
-        std::cout << "Observed: " << h->Integral() << std::endl;
-        std::cout << "--------------------" << std::endl;
-      }
-
-    }
-
-
-    void Stack(TString name)
-    {
-      //TODO: C'mon dude, that should be easy 
-    }
-
-
-  private:
-
-    TString data;
-    std::vector <Selector> listOfSelectors;
-    //Selector ds();
-    std::vector <Int_t> listOfColors;
-
-
+  for (int i = 0; i < bkgs.size(); i++)
+  {
+    Selector sel(pathToFiles, bkgs[i], prefixForHistos);
+    listOfSelectors.push_back(sel); 
+    listOfColors.push_back(i+1);
+  }
+  
+  if (data != "")
+    dataSelector = new Selector(pathToFiles, data, prefixForHistos);
 }
+
+
+void Plotter::SetLegendPos(Float_t x1, Float_t y1, Float_t x2, Float_t y2)
+{
+  fLegX1 = x1; 
+  fLegX2 = x2; 
+  fLegY1 = y1; 
+  fLegY2 = y2; 
+}
+
+    
+void Plotter::SetLegendSize(Float_t size = 0.065)
+{
+  LegendTextSize = size;
+}
+
+
+void Plotter::SetColors(std::vector<Int_t> col)
+{
+  listOfColors = col;
+}
+
+
+void Plotter::SetTitle(TString _title)
+{
+  title = _title;
+}
+
+    
+void Plotter::SetXTitle(TString _xtitle)
+{
+  xtitle = _xtitle;
+}
+
+
+void Plotter::SetYTitle(TString _ytitle)
+{
+  ytitle = _ytitle;
+}
+
+
+TH1F* Plotter::GetHisto(TString name) //name is the name of the histo
+{
+    //TODO: I don't work for a specific process!
+    //TODO: Number of histos could depend on the process (?)
+    //FIXME: Algo me dice que esto solo devuelve la primera coincidencia.
+        
+    int numberOfSelectors = listOfSelectors.size();
+    int numberOfHistos = listOfSelectors[0].GetNumberOfHistos();
+
+    for (int i = 0; i < numberOfSelectors; i++)
+    {
+      for (int j = 0; i < numberOfHistos; i++)
+      {
+        TString n = listOfSelectors[i].histograms[j]->GetName(); //full name
+
+        if (!std::string(n).find(std::string(name))){continue;}
+        //si no encuentras el histo, sigue adelante
+        return listOfSelectors[i].histograms[j];
+      }
+    }
+    throw;
+}
+
+int Plotter::GetEvents(TString name)
+{
+  // Returns the integral of a histogram
+  return GetHisto(name)->Integral();
+}
+
+void Plotter::PrintEvents(TString name)
+{
+  // Name is a name. Not a list or any other weird thing.
+
+  std::cout << "\nPrinting number of events for histogram " << name << "\n";
+  std::cout << "---------------------------------------------" << std::endl;
+
+  Int_t totalEvts = 0;
+
+  for (int i = 0; i < listOfSelectors.size(); i++)
+  {
+    TH1F* h = listOfSelectors[i].GetHisto(name);
+
+    std::cout << name << ": " << h->Integral();
+    totalEvts += h->Integral();
+  }
+
+  std::cout << "Total expected: " << totalEvts << std::endl;
+  std::cout << "--------------------" << std::endl;
+
+  if (data != "")
+  {
+    TH1F* h = dataSelector->GetHisto(name);
+    std::cout << "Observed: " << h->Integral() << std::endl;
+    std::cout << "--------------------" << std::endl;
+  }
+}
+
+
+void Plotter::Stack(TString name)
+{
+  // name is NOT a list
+      
+  TCanvas *c = new TCanvas("c", "canvas", 800, 800);
+  TLegend leg = TLegend(fLegX1, fLegY1, fLegX2, fLegY2); 
+  leg.SetTextSize(LegendTextSize);
+  leg.SetBorderSize(0);
+  leg.SetFillColor(10);
+
+  THStack *hs = new THStack("hstack_" + name, "hstack");
+      
+  // fill hstack >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  for (int i = 0; i < listOfSelectors.size(); i++)
+  {
+    TH1F* h = GetHisto(name); // haha don't make me laugh
+
+    h->SetFillColor(listOfColors[i]);
+    h->SetLineColor(0);
+    hs->Add(h);
+    leg.AddEntry(h, listOfSelectors[i].prefix, "hstack");
+  }
+
+  hs->Draw("hist");
+
+  if (title  != "") {hs->SetTitle(title);}
+  if (xtitle != "") {hs->GetXaxis()->SetTitle(title);}
+  if (ytitle != "") {hs->GetYaxis()->SetTitle(ytitle);}
+  hs->GetYaxis()->SetTitleOffset(1.35);
+
+  TH1 *aux = ((TH1*)(hs->GetStack()->Last()));
+  Float_t max = aux->GetMaximum(); // why do I have to do this in 2 lines?
+
+  if (data != "")
+  {
+    TH1F* hdata = dataSelector->GetHisto(name); // 笑わせるな...
+    hdata->SetMarkerStyle(20);
+    hdata->SetMarkerColor(1);
+    hdata->Draw("pesame");                      //これは何か 笑
+
+    Float_t maxData = hdata->GetMaximum();
+    if (max < maxData) {max = maxData;}
+
+    leg.AddEntry(hdata, dataSelector->prefix, "p");
+  }
+
+  leg.Draw("same");
+  hs->SetMaximum(max*1.1);
+  c->Print(name + ".png", "png");
+}
+
+
+
+
--- selector.cpp
+++ selector.cpp
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 #include "TH1.h"
 #include "TFile.h"
 #include "TTree.h"
@@ -17,131 +16,120 @@
 // TODO: aplicar cortes
 
 
-class Selector
+
+
+Selector::Selector(std::string _filePath, std::string _fileName, std::string _prefix)
 {
+  if (!fileExist(_filePath + _fileName))
+  {
+    std::cout << "[Selector] ERROR. File '" << filePath + fileName << "' "
+    "does not exist!" << std::endl;
 
-  public:
+    throw;
+  }
+  fileName = TString(_fileName);  // including .root
+  filePath = TString(_filePath);  // including last '/'
+  prefix = TString(_prefix);      //
 
-    std::vector<TH1F*> histograms; 
+  CreateHistograms();
+  Loop();
+}
 
-    Selector(std::string _filePath, std::string _fileName, std::string _prefix)
+TH1F* Selector::GetHisto(TString name) //name is the name of the histo
+{
+    int len = histograms.size();
+    for (int i = 0; i < len; i++)
     {
-      if (!fileExist(_filePath + _fileName))
-      {
-        std::cout << "[Selector] ERROR. File '" << filePath + fileName << "' "
-        "does not exist!" << std::endl;
-
-        throw;
-      }
-      fileName = TString(_fileName);  // including .root
-      filePath = TString(_filePath);  // including last '/'
-      prefix = TString(_prefix);      // including last '/'
-
-      CreateHistograms();
-      Loop();
-    }
-
-    TH1F* GetHisto(TString name) //name is the name of the histo
-    {
-        int len = histograms.size();
-        for (int i = 0; i < len; i++)
-        {
-          TString n = histograms[i]->GetName(); //full name
+      TString n = histograms[i]->GetName(); //full name
             
-          if ((prefix + '_' + name) == n)
-            return histograms[i];
-        }
-
-        std::cout << "[Selector::GetHisto] WARNING: not found histo named " + \
-        name + ". Keep up the good work!" << std::endl;
-
-        throw;
+      if ((prefix + '_' + name) == n)
+        return histograms[i];
     }
 
-    int GetNumberOfHistos()
-    {
-      return histograms.size();
-    }
+    std::cout << "[Selector::GetHisto] WARNING: not found histo named " + \
+    name + ". Keep up the good work!" << std::endl;
 
-  private:
+    throw;
+}
 
-    TString prefix;                 // prefix for ALL histos
-    TString fileName;               // name of the root file
-    TString filePath;               // path of the root file
+int Selector::GetNumberOfHistos()
+{
+  return histograms.size();
+}
+
 
   
-    bool fileExist(const std::string name)
-    {
-      //std::cout << name << std::endl;
-      return (access(name.c_str(), F_OK) != -1);
-    }
+bool Selector::fileExist(const std::string name)
+{
+  //std::cout << name << std::endl;
+  return (access(name.c_str(), F_OK) != -1);
+}
 
-    void CreateHistograms()
-    {
-      TH1F* h1 = new TH1F(prefix + TString("_MuonPt"), ";p_{T}^{#mu} (GeV);Events", 20, 0, 200);
-      TH1F* h2 = new TH1F(prefix + TString("_DiMuonMass"), ";m^{#mu#mu} (GeV);Events", 20, 0, 200);
+void Selector::CreateHistograms()
+{
+  TH1F* h1 = new TH1F(prefix + TString("_MuonPt"), ";p_{T}^{#mu} (GeV);Events", 20, 0, 200);
+  TH1F* h2 = new TH1F(prefix + TString("_DiMuonMass"), ";m^{#mu#mu} (GeV);Events", 20, 0, 200);
 
-  	  histograms.push_back(h1); 
-  	  histograms.push_back(h2);    
+  histograms.push_back(h1); 
+  histograms.push_back(h2);    
 
-      //add here more histograms   
-    }
+  //add here more histograms   
+}
 
-    void Loop()
-    { 
-      TFile f(filePath + fileName);
-      TTree * tree = (TTree *) f.Get("events");
+void Selector::Loop()
+{ 
+  TFile f(filePath + fileName);
+  TTree * tree = (TTree *) f.Get("events");
 
-      Int_t numEvents = tree->GetEntries();
+  Int_t numEvents = tree->GetEntries();
 
-      // Set Adresses to branches >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-      Int_t NMuon;
-      Bool_t triggerIsoMu24;
-      Float_t Muon_Px[3]; // just in case... memory is not a problem here
-      Float_t Muon_Py[3];
-      Float_t Muon_Pz[3];
-      Float_t Muon_E[3];
-      Float_t EventWeight;
+  // Set Adresses to branches >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  Int_t NMuon;
+  Bool_t triggerIsoMu24;
+  Float_t Muon_Px[3]; // just in case... memory is not a problem here
+  Float_t Muon_Py[3];
+  Float_t Muon_Pz[3];
+  Float_t Muon_E[3];
+  Float_t EventWeight;
 
-      tree->SetBranchAddress("NMuon", &NMuon);
-      tree->SetBranchAddress("Muon_Px", &Muon_Px);
-      tree->SetBranchAddress("Muon_Py", &Muon_Py);
-      tree->SetBranchAddress("Muon_Pz", &Muon_Pz);
-      tree->SetBranchAddress("Muon_E", &Muon_E);
-      tree->SetBranchAddress("triggerIsoMu24", &triggerIsoMu24);
-      tree->SetBranchAddress("EventWeight", &EventWeight);
-      // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  tree->SetBranchAddress("NMuon", &NMuon);
+  tree->SetBranchAddress("Muon_Px", &Muon_Px);
+  tree->SetBranchAddress("Muon_Py", &Muon_Py);
+  tree->SetBranchAddress("Muon_Pz", &Muon_Pz);
+  tree->SetBranchAddress("Muon_E", &Muon_E);
+  tree->SetBranchAddress("triggerIsoMu24", &triggerIsoMu24);
+  tree->SetBranchAddress("EventWeight", &EventWeight);
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-      // Loop over every event >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-      for (int i = 0; i < numEvents; i++)
-      {
-        tree->GetEntry(i);
+  // Loop over every event >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  for (int i = 0; i < numEvents; i++)
+  {
+    tree->GetEntry(i);
 
-        if (!triggerIsoMu24) {continue;}
-        if (NMuon != 1) {continue;} 
+    if (!triggerIsoMu24) {continue;}
+    if (NMuon != 1) {continue;} 
 
-        TLorentzVector muon;
-        muon.SetPxPyPzE(Muon_Px[0], Muon_Py[0], Muon_Pz[0], Muon_E[0]);
+    TLorentzVector muon;
+    muon.SetPxPyPzE(Muon_Px[0], Muon_Py[0], Muon_Pz[0], Muon_E[0]);
         
-        Float_t weight;
-        if (fileName != "data") {weight = EventWeight;} else weight = 1;
+    Float_t weight;
+    if (fileName != "data") {weight = EventWeight;} else weight = 1;
 
-        // full analysis >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    // full analysis >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   
 
-        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-        // fill histos >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        GetHisto("MuonPt")->Fill(muon.Pt()); //C'mon Mario, you can do it better 
+    // fill histos >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    GetHisto("MuonPt")->Fill(muon.Pt()); //C'mon Mario, you can do it better 
 
-        // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-      }
-
-
-    }
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  }
 
 
-};
+}
+
+
 
 
 void selector()
