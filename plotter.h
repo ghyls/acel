b070e96ee55017096c8b20c75634abd9bf45dbf3
Mario <marioyales2@gmail.com>
2019-02-28 13:24:54 +0100

    I love you more than myself

--- plotter.h
+++ plotter.h
@@ -15,15 +15,17 @@ class Plotter
 {
     public:
 
+        Selector* dataSelector;
+
         Float_t fLegX1, fLegY1, fLegX2, fLegY2;
 
         Float_t LegendTextSize;
-        TString xtitle;
-        TString ytitle;
-        TString title; 
+        TString xtitle = "";
+        TString ytitle = "";
+        TString title = ""; 
 
         Plotter(std::vector<std::string> bkgs, std::string pathToFiles,  
-               std::string prefixForHistos, std::string data = "");
+               std::string prefixForHistos, std::string data);
 
         void SetLegendPos(Float_t x1, Float_t y1, Float_t x2, Float_t y2);
         void SetLegendSize(Float_t size = 0.065);
