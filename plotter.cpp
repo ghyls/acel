c03b5a0e8832d3a2b0ec2f9c7e73373096b53b6e
Mario <marioyales2@gmail.com>
2019-03-01 00:42:40 +0100

    added '2 histo with ratio' functionality

--- plotter.cpp
+++ plotter.cpp
@@ -9,7 +9,7 @@
 #include "TCanvas.h"
 #include "TLegend.h"
 #include "THStack.h"
-
+#include "TLine.h"
 
 
 
@@ -37,14 +37,14 @@ std::string prefixForHistos, std::string data_)
   
   if (data_ != "")
   {
-
-    std::cout <<"hi :D" << std::endl;
-    //Selector *dataS = new Selector(pathToFiles, data_, prefixForHistos);
     dataSelector = new Selector(pathToFiles, data_, prefixForHistos);
-    //Selector dataSelector(pathToFiles, data_, prefixForHistos);
   }
 }
 
+Plotter::~Plotter()
+{
+  std::cout << "destroyed Plotter" << std::endl;
+}
 
 void Plotter::SetLegendPos(Float_t x1, Float_t y1, Float_t x2, Float_t y2)
 {
@@ -128,7 +128,7 @@ void Plotter::PrintEvents(TString name)
   {
     TH1F* h = listOfSelectors[i].GetHisto(name);
 
-    std::cout << name << ": " << h->Integral() << std::endl;
+    std::cout << listOfSelectors[i].process <<": "<< h->Integral() << std::endl;
     totalEvts += h->Integral();
   }
 
@@ -143,8 +143,116 @@ void Plotter::PrintEvents(TString name)
   }
 }
 
+void Plotter::plotWithRatio(TString nameH1, TString nameH2)
+{
 
-void Plotter::Stack(TString name)
+  TH1F* h1 = listOfSelectors[0].GetHisto(nameH1);
+  TH1F* h2 = listOfSelectors[0].GetHisto(nameH2);
+
+  //do Plots
+  TCanvas *c = new TCanvas("c", "canvas", 800, 800);
+
+  // Normalizamos los histogramas a 1
+  // h1->Scale(1/h1->Integral());
+  // h2->Scale(1/h2->Integral());
+
+
+  // Define Pads >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // See https://root.cern.ch/root/html/tutorials/hist/ratioplot.C.html
+
+  // Upper plot will be in pad1. 
+  TPad *pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.0);
+  pad1->SetBottomMargin(0.05); 
+  pad1->SetGridx();         // Vertical grid
+  pad1->Draw();             // Draw the upper pad: pad1
+  pad1->cd();               // pad1 becomes the current pad
+  h1->SetStats(0);          // No statistics on upper plot
+  h1->Draw("hist");         // Draw h1
+  h2->Draw("same, hist");   // Draw h2 on top of h1
+  
+  // lower plot will be in pad2
+  c->cd();          // Go back to the main canvas before defining pad2
+  TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
+  pad2->SetTopMargin(0.05);
+  pad2->SetBottomMargin(0.2);
+  pad2->SetGridx(); // vertical grid
+  pad2->Draw();
+  pad2->cd();       // pad2 becomes the current pad
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+  // Define the ratio plot >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  TH1F *h3 = (TH1F*)h1->Clone("h3");
+  h3->SetLineColor(kBlack);
+  h3->SetMinimum(0);    // Define Y ..
+  h3->SetMaximum(3);    // .. range
+  h3->Sumw2();
+  h3->SetStats(0);      // No statistics on lower plot
+  h3->Divide(h2);       // ~ h1/h2
+  h3->SetMarkerStyle(21);
+  h3->Draw("ep");       // Draw the ratio plot
+
+  TLine *l = new TLine(10, 1.0, 120, 1.0);  // horizontal line
+  l->SetLineColor(kBlue);
+  l->Draw();
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  
+  // Cosmetics >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  h1->SetLineColor(kAzure+2);
+  h2->SetLineColor(kRed+1);
+  h1->SetLineWidth(2);
+  h2->SetLineWidth(2);
+
+  // Ejes y otras opciones... (solo en el primer histograma que pintamos)
+  h1->SetStats(0);
+  h1->SetTitle(title);
+  h1->GetYaxis()->SetTitle("Events");
+  h1->GetYaxis()->SetTitleOffset(1.2);
+  h1->GetYaxis()->SetLabelFont(43);
+  h1->GetYaxis()->SetLabelSize(15);
+  h1->GetXaxis()->SetLabelFont(43);
+  h1->GetXaxis()->SetLabelSize(15);
+
+  // Leyenda
+  pad1->cd();  
+  TLegend leg = TLegend(fLegX1, fLegY1, fLegX2, fLegY2); // x0, y0, x1, y1
+  leg.AddEntry(h1, nameH1, "l");
+  leg.AddEntry(h2, nameH2, "l");
+  leg.SetTextSize(0.043);
+  leg.Draw();
+
+  // Ratio Plot settings -------------------------------------------------------
+  h3->SetTitle(""); // Remove the ratio title
+
+  // Y axis
+  h3->GetYaxis()->SetTitle("ratio");
+  h3->GetYaxis()->SetTitleSize(20);
+  h3->GetYaxis()->SetTitleFont(43);
+  h3->GetYaxis()->SetTitleOffset(1.55);
+  h3->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+  h3->GetYaxis()->SetLabelSize(15);
+
+  // X axis
+  h3->GetXaxis()->SetTitle(xtitle);
+  h3->GetXaxis()->SetTitleSize(20);
+  h3->GetXaxis()->SetTitleFont(43);
+  h3->GetXaxis()->SetTitleOffset(4.);
+  h3->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+  h3->GetXaxis()->SetLabelSize(15);
+  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+
+
+  // Guardamos la figura como .png y .pdf
+  c->Print("DiMuPt.png", "png");
+}
+
+
+
+void Plotter::Stack(TString name, Float_t maxY)
 {
   // name is NOT a list
       
@@ -152,23 +260,22 @@ void Plotter::Stack(TString name)
   TLegend leg = TLegend(fLegX1, fLegY1, fLegX2, fLegY2); 
   leg.SetTextSize(LegendTextSize);
   leg.SetBorderSize(0);
-  //leg.SetFillColor(10);
+  leg.SetFillColor(10);
 
   THStack *hs = new THStack("hstack_" + name, "hstack");
       
   // fill hstack >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   for (unsigned int i = 0; i < listOfSelectors.size(); i++)
   {
-    //TH1F* h = GetHisto(listOfSelectors[i].histoName); // TODO: DON'T MAKE ME LAUGH
     TH1F* h = listOfSelectors[i].GetHisto(name); // TODO: DON'T MAKE ME LAUGH
     
-    std::cout << "name: " << h->GetName() << std::endl;
-    std::cout << "integral " << h->Integral() << std::endl;
-
+    //std::cout << "name: " << h->GetName() << std::endl;
+    //std::cout << "integral " << h->Integral() << std::endl;
+  
     h->SetFillColor(listOfColors[i]);
     h->SetLineColor(kBlack);
     hs->Add(h);
-    leg.AddEntry(h, listOfSelectors[i].histoName, "f");
+    leg.AddEntry(h, listOfSelectors[i].process, "f");
   }
 
   hs->Draw("hist");
@@ -186,7 +293,7 @@ void Plotter::Stack(TString name)
     TH1F* hdata = dataSelector->GetHisto(name); 
     hdata->SetMarkerStyle(20);
     hdata->SetMarkerColor(kTeal+1); // 1
-    hdata->Draw("pesame");                      //これは何か 笑
+    hdata->Draw("pesame");
 
     Float_t maxData = hdata->GetMaximum();
     if (max < maxData) {max = maxData;}
@@ -195,7 +302,8 @@ void Plotter::Stack(TString name)
   }
   
   leg.Draw("same");
-  hs->SetMaximum(max*1.1);
+  if (maxY == -1){hs->SetMaximum(max*1.1);}
+  else{hs->SetMaximum(maxY);}
   c->Print(name + ".png", "png");
 }
 
