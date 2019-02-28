6aff0074a81268bea93518ef2bb3414139caa1b9
Mario <marioyales2@gmail.com>
2019-03-01 00:43:14 +0100

    even more lethal

--- terminator.cpp
+++ terminator.cpp
@@ -15,11 +15,8 @@
 #include "TROOT.h"
 
 
-
 void terminator()
 {
-
-    std::cout <<"[terminator] NOTE. Hello!" << std::endl;
 
     gROOT->LoadMacro("selector.cpp+");
     gROOT->LoadMacro("plotter.cpp+");
@@ -32,23 +29,46 @@ void terminator()
     std::string prefix = "hello";    
 
     std::vector<std::string> MCsamples;
-    MCsamples = {"qcd", "wjets", "ww", "wz", "zz", "dy", "single_top", "ttbar"};
+
 
     std::vector<Int_t> colors = {kGray, kBlue-1, kTeal-1, kTeal+1, kTeal+4, \
                                 kAzure-8, kOrange+1, kRed+1};
 
-    Plotter plot(MCsamples, pathToFiles, prefix, "data");
+    MCsamples = {"qcd", "wjets", "ww", "wz", "zz", "dy", "single_top", "ttbar"};
+    Plotter * plot;
 
+
+    // mu mu Pt >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    plot = new Plotter(MCsamples, pathToFiles, prefix, "data");
     //plot.SetColors(colors);
-    plot.SetLegendPos(0.62, 0.6, 0.88, 0.88);
-    plot.SetYTitle("Events");
-    plot.SetTitle("HELLO");
-    plot.SetXTitle("mumuM");
+    plot->SetTitle("Muon Pt");
+    plot->SetXTitle("muonPt");
+    plot->SetLegendPos(0.62, 0.6, 0.85, 0.85); plot->SetYTitle("Events");
+    plot->Stack("MuonPt");
 
-    plot.Stack("MuonPt");
-    //plot.Stack("DiMuonMass");
+    plot->PrintEvents("MuonPt");
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    // Jet btag >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    plot = new Plotter({"wjets", "ttbar"}, pathToFiles, prefix, "");
+    plot->SetTitle("Muon Pt");
+    plot->SetXTitle("Jet_btag");
+    plot->SetLegendPos(0.7, 0.8, 0.88, 0.88); plot->SetYTitle("Events");
+    plot->Stack("Jet_btag", 1000);
+
+    plot->PrintEvents("Jet_btag");
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
-    plot.PrintEvents("MuonPt");
+    // Trigger eff. >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    plot = new Plotter({"ttbar"}, pathToFiles, prefix, "");
+    plot->SetTitle("Eficiencia del trigger");
+    plot->SetXTitle("Ptmuon");
+    plot->SetLegendPos(0.7, 0.75, 0.9, 0.9); plot->SetYTitle("Events");
 
+    plot ->plotWithRatio("AllMuons", "MuonPt");
+
+    plot->PrintEvents("AllMuons");
+    plot->PrintEvents("MuonPt");
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 }
