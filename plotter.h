23f6d4f0524802e556ed4a5afc02e8277b3af188
Mario <marioyales2@gmail.com>
2019-03-08 14:02:10 +0100

    I don't know what I did here

--- plotter.h
+++ plotter.h
@@ -24,7 +24,7 @@ class Plotter
         TString xtitle = "";
         TString ytitle = "";
         TString title = ""; 
-
+        TString outName = "out"; 
 
         Plotter();
         Plotter(std::vector<std::string> bkgs, std::string pathToFiles, 
@@ -38,12 +38,14 @@ class Plotter
         void SetTitle(TString _title);
         void SetXTitle(TString _xtitle);
         void SetYTitle(TString _ytitle);
+        void SetTitles(TString _title, TString _xtitle, TString _ytitle, 
+                        TString _outName);
         TH1F* GetHisto(TString name);
         int GetEvents(TString name);
         void PrintEvents(TString name);
-        void Stack(TString name, Float_t maxY = -1);
-        void plotWithRatio(TString nameH1, TString nameH2, TString title, \
-                            TString rLabel, float rMin, float rMax, float max = 999);
+        void Stack(TString name,  bool doLogY = 0, Float_t maxY = -1);
+        void plotWithRatio(TString nameH1, TString nameH2, \
+        TString rLabel, float rMin, float rMax, bool doLogY = 0, float max = 999);
         void DrawOverflowBin(TH1F* h);
         void GetTriggerEff();
 
