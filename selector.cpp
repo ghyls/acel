5ee414b97d1d0d171af02b9cb1c60cb8f0474f46
Mario <marioyales2@gmail.com>
2019-03-09 02:41:31 +0100

    added more histograms and improved the analysis

--- selector.cpp
+++ selector.cpp
@@ -18,6 +18,11 @@
 // TODO: aplicar cortes
 
 
+#define JET_MIN_PT 30 // 30
+#define JET_MAX_PT 900
+#define MUON_MIN_PT 26.7
+
+
 
 
 Selector::Selector(std::string _filePath, std::string _fileName)
@@ -86,12 +91,18 @@ void Selector::CreateHistograms(TString prefix)
   //there variables may be called from other cpps.
   TH1F* h1 = new TH1F(prefix + TString("_MuonPt"), "", 20, 20, 120);
   TH1F* h2 = new TH1F(prefix + TString("_Jet_btag"), "", 80, -2, 6);
-  TH1F* h3 = new TH1F(prefix + TString("_MuonPt_raw"), "", 20, 10, 120);
+  TH1F* h3 = new TH1F(prefix + TString("_MuonPt_raw"), "", 40, 10, 60);
   TH1F* h4 = new TH1F(prefix + TString("_Acep_gen"), "", 20, -1, 2);
   TH1F* h5 = new TH1F(prefix + TString("_Acep_obs"), "", 20, -1, 2);
-  TH1F* h6 = new TH1F(prefix + TString("_MuMuMass"), "", 60, 60, 120);
+  TH1F* h6 = new TH1F(prefix + TString("_MuMuMass"), "", 40, 60, 120);
   TH1F* h7 = new TH1F(prefix + TString("_Muon_Iso"), "", 60, 0, 0.2);
-  TH1F* h8 = new TH1F(prefix + TString("_MuonPt_TriggOnly"), "", 20, 10, 120);
+  TH1F* h8 = new TH1F(prefix + TString("_MuonPt_TriggOnly"), "", 40, 10, 60);
+
+  TH1F* h9 = new TH1F(prefix + TString("_BJet_Pt"), "", 20, 20, 160);
+  TH1F* h10 = new TH1F(prefix + TString("_Hadr_BJet_Pt_GEN"), "", 20, 20, 160);
+  TH1F* h11 = new TH1F(prefix + TString("_Lept_BJet_Pt_GEN"), "", 20, 20, 160);
+  TH1F* h12 = new TH1F(prefix + TString("_Jets_GEN_Pt"), "", 20, 20, 160);
+  TH1F* h13 = new TH1F(prefix + TString("_TempXSec"), "", 20, 0, 2);
 
   histograms.push_back(h1); 
   histograms.push_back(h2); 
@@ -101,6 +112,11 @@ void Selector::CreateHistograms(TString prefix)
   histograms.push_back(h6); 
   histograms.push_back(h7); 
   histograms.push_back(h8); 
+  histograms.push_back(h9); 
+  histograms.push_back(h10); 
+  histograms.push_back(h11); 
+  histograms.push_back(h12); 
+  histograms.push_back(h13); 
 }
 
 void Selector::Loop()
@@ -116,6 +132,15 @@ void Selector::Loop()
   Float_t Jet_E[20]; 
   Float_t Jet_btag[20]; 
   Float_t Jet_Px[20], Jet_Py[20], Jet_Pz[20];
+
+  Float_t MChadronicBottom_px; // if filled, there are 1 b tags.
+  Float_t MChadronicBottom_py; // if filled, there are 1 b tags.
+  Float_t MChadronicBottom_pz; // if filled, there are 1 b tags.
+
+  Float_t MCleptonicBottom_px; // if filled, there are 2 b tags.
+  Float_t MCleptonicBottom_py; // if filled, there are 2 b tags.
+  Float_t MCleptonicBottom_pz; // if filled, there are 2 b tags.
+
   Bool_t triggerIsoMu24;
   Float_t Muon_Px[5]; // just in case... memory is not a problem here
   Float_t Muon_Py[5];
@@ -147,12 +172,22 @@ void Selector::Loop()
   tree->SetBranchAddress("MClepton_py", &MClepton_py);
   tree->SetBranchAddress("MClepton_pz", &MClepton_pz);
   tree->SetBranchAddress("MCleptonPDGid", &MCleptonPDGid);
+
+  tree->SetBranchAddress("MChadronicBottom_px", &MChadronicBottom_px);
+  tree->SetBranchAddress("MChadronicBottom_py", &MChadronicBottom_py);
+  tree->SetBranchAddress("MChadronicBottom_pz", &MChadronicBottom_pz);
+  tree->SetBranchAddress("MCleptonicBottom_px", &MCleptonicBottom_px);
+  tree->SetBranchAddress("MCleptonicBottom_py", &MCleptonicBottom_py);
+  tree->SetBranchAddress("MCleptonicBottom_pz", &MCleptonicBottom_pz);
+
   // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
   // Loop over every event >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   std::cout << "[Selector::loop]: " << process << ", " << numEvents  << \
                 " events." << std::endl;
 
+
+  float nObs = 0; // TODO: something with me
   for (int i = 0; i < numEvents; i++)
   {
     tree->GetEntry(i);
@@ -189,7 +224,7 @@ void Selector::Loop()
     // Z mass >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     if (NIsoMuon > 1 && triggerIsoMu24)
     {
-      if(leadMuon.Pt() > 25)//|| muon2.Pt() > 20)
+      if(leadMuon.Pt() > MUON_MIN_PT)//|| muon2.Pt() > 20)
       {
         GetHisto("MuMuMass")->Fill((leadMuon + muon2).M(), EventWeight);
       }
@@ -208,7 +243,7 @@ void Selector::Loop()
     {
       muon.SetPxPyPzE(MClepton_px[leadMuIndex], MClepton_py[leadMuIndex], MClepton_pz[leadMuIndex], 0);
   
-      if (muon.Pt() > 26.7)
+      if (muon.Pt() > MUON_MIN_PT)
       {
         GetHisto("Acep_gen")->Fill(0);
       }
@@ -222,19 +257,19 @@ void Selector::Loop()
 
     GetHisto("MuonPt_raw")->Fill(leadMuon.Pt(), weight);
 
-
+     
     if (triggerIsoMu24) 
     {
       GetHisto("MuonPt_TriggOnly")->Fill(leadMuon.Pt(), weight);
 
       // Aceptancia >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-      if (leadMuon.Pt() > 26.7)
+      if (leadMuon.Pt() > MUON_MIN_PT)
       {
         GetHisto("Acep_obs")->Fill(0);
       }
       // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-      if (NJet > 0 && NJet < 10)
+      if (NJet > 0)
       {
         for (int j = 0; j < NJet; j++)
           {
@@ -242,27 +277,68 @@ void Selector::Loop()
           }
       }
       
-      if (leadMuon.Pt() > 26.7)
+      if (leadMuon.Pt() > MUON_MIN_PT)
       {
-        static int bJets = 0;      
-        static int nJetWithBigPt = 0;    // useless  
-        if (NJet > 3)  
+        int bJets = 0;    
+        std::vector <int> indexBJets = {}; 
+        int nJetWithBigPt = 0;    // useless  
+        if (NJet > 1)  
         {
           for (int j = 0; j < NJet; j++)
           {
             jet.SetPxPyPzE(Jet_Px[j], Jet_Py[j], Jet_Pz[j], Jet_E[j]);
             
-            if (Jet_btag[j] > 2 && jet.Pt() > 20)
+            if (Jet_btag[j] > 0 && jet.Pt() > JET_MIN_PT)
             {
               bJets ++;
+              indexBJets.push_back(j);
             }
             
           }
-          if (bJets > 0)// && nJetWithBigPt > 0)
+          if (bJets > 0)
           {
+            nObs += weight;
             GetHisto("MuonPt")->Fill(leadMuon.Pt(), weight); 
+            GetHisto("TempXSec")->Fill(1, weight); 
+            
           }  
-        }    
+        } 
+
+        for (int j = 0; j < bJets; j++)
+        {
+          jet.SetPxPyPzE(Jet_Px[indexBJets[j]], Jet_Py[indexBJets[j]], \
+                            Jet_Pz[indexBJets[j]], Jet_E[indexBJets[j]]);
+          if (jet.Pt() > JET_MIN_PT && jet.Pt() < JET_MAX_PT)
+            GetHisto("BJet_Pt")->Fill(jet.Pt(), weight); 
+        }
+
+
+        TLorentzVector JetHadrGEN;
+        TLorentzVector JetLeptGEN;
+
+        JetHadrGEN.SetPxPyPzE(MChadronicBottom_px, MChadronicBottom_py, \
+                            MChadronicBottom_pz, INFINITY);
+
+        JetLeptGEN.SetPxPyPzE(MCleptonicBottom_px, MCleptonicBottom_py, \
+                            MCleptonicBottom_pz, INFINITY);
+
+        GetHisto("Hadr_BJet_Pt_GEN")->Fill(JetHadrGEN.Pt(), weight);
+        GetHisto("Lept_BJet_Pt_GEN")->Fill(JetLeptGEN.Pt(), weight);
+
+        if (JetHadrGEN.Pt() > JET_MIN_PT && JetHadrGEN.Pt() < JET_MAX_PT)
+        {
+          GetHisto("Jets_GEN_Pt")->Fill(JetHadrGEN.Pt(), weight);
+        }
+        if (JetLeptGEN.Pt() > JET_MIN_PT && JetLeptGEN.Pt() < JET_MAX_PT)
+        {
+          GetHisto("Jets_GEN_Pt")->Fill(JetLeptGEN.Pt(), weight);
+        }
+
+        // para cada bJet, llenar el histo de jets con su pt. Luego, hacer lo
+        // mismo con otro histo con el Pt del numero de jets de la generacion.
+        // proceder igual que con trigg eff. Para cada valor del momento, y para
+        // cada valor de btag_lim pintar un histo en 2d con la eficiencia.
+
       }
     }
 
@@ -272,6 +348,6 @@ void Selector::Loop()
   }
 
 
-
+  //std::cout << "counted " << nObs << std::endl;
 }
 
