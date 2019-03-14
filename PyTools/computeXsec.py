c58fbf15799d319e3a78b66dc8dec2786b3592ea
Mario <marioyales2@gmail.com>
2019-03-14 14:13:16 +0100

    this is almost ready

--- PyTools/computeXsec.py
+++ PyTools/computeXsec.py
@@ -21,19 +21,83 @@ class Data:
 
 M = np.loadtxt("xsec.dat")
 
+RAW_bTAgEff = M[0]      # medidos           | generados
+RAW_trigEff = M[1]      # pasan Trigger     | no lo pasan
+RAW_aceptan = M[2]      # datos observados  | generados
+
+totalData = M[3][0]
+totalBkg = M[3][1]
+
+# designaremos con MIN al numerador (ej. RAW_bTagEff[0]) y con MAX al
+# denominador (ej. RAW_bTagEff[1]) del cociente que de vuelve cada valor (ej. la
+# eficiencia en b tag).
 
 
+MIN_bTAgEff = RAW_bTAgEff[0]
+MAX_bTAgEff = RAW_bTAgEff[1]
+
+MIN_trigEff = RAW_trigEff[0]
+MAX_trigEff = RAW_trigEff[1]
+
+MIN_aceptan = RAW_aceptan[0]
+MAX_aceptan = RAW_aceptan[1]
+
+# inicializamos todas las variables >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+MIN_bTAgEff = sp.Symbol("MIN_bTAgEff")
+MAX_bTAgEff = sp.Symbol("MAN_bTAgEff")
+
+MIN_trigEff = sp.Symbol("MIN_trigEff")
+MAX_trigEff = sp.Symbol("MAN_trigEff")
+
+MIN_aceptan = sp.Symbol("MIN_aceptan")
+MAX_aceptan = sp.Symbol("MAN_acept")
+
+N = sp.Symbol("N")
+B = sp.Symbol("B")
+
+muonEff = sp.Symbol("muonEff")
+lumi = sp.Symbol("lumi")
 
 
+# y las creamos como objetos de Data, con su error poissoniano >>>>>>>>>>>>>>>>>
+
+variables =    [[MIN_bTAgEff, Data(RAW_bTAgEff[0], RAW_bTAgEff[0]**0.5)], 
+                [MAX_bTAgEff, Data(RAW_bTAgEff[1], RAW_bTAgEff[1]**0.5)],
+                [MIN_trigEff, Data(RAW_trigEff[0], RAW_trigEff[0]**0.5)],
+                [MAX_trigEff, Data(RAW_trigEff[1], RAW_trigEff[1]**0.5)],
+                [MIN_aceptan, Data(RAW_aceptan[0], RAW_aceptan[0]**0.5)],
+                [MAX_aceptan, Data(RAW_aceptan[1], RAW_aceptan[1]**0.5)],
+                [N, Data(totalData, totalData**0.5)],
+                [B, Data(totalBkg, totalBkg**0.5)],
+                [muonEff, Data(0.99, 0.01)],
+                [lumi, Data(50, 5)]]
+
+# qué son las incertidumbres de normalización? 
+# definimos algunos valores más, sin error asociado. >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+BRTToMuB = 0.134
+BRTToTauB = 0.71
+BRTauToMu = 0.1739
+BRTTo2JB = 0.665
+
+BRTTbarToMu = (BRTToMuB + BRTToTauB * BRTauToMu) * BRTTo2JB
+
+# definimos las cantidades que introducimos en la fórmula de Xsec >>>>>>>>>>>>>>
+
+bTagEff = MIN_bTAgEff / MAX_bTAgEff   # en datos
+trigEff = MIN_trigEff / MAX_trigEff
+acept = MIN_aceptan / MAX_aceptan
 
 
-m = sp.Symbol('m')
-V = sp.Symbol('V')
+# y las incertidumbres de las eficiencias?
+#   trigger -> (1-eff)/2
+#   bt -> eff datos - eff MC
 
 
-variables = [[m, Data(3, 0.8)], [V, Data(4.5, 0.4)]]   # [m, V]
+# definimos la formula que devolverá la Xsec >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-func = m/V
+func = (N-B) / (lumi * acept * trigEff * muonEff * bTagEff * BRTTbarToMu)
 
 # ------------------------------------------------------------------------------
 
@@ -63,7 +127,7 @@ def computeValue(func, variables):
 error = computeError(func, variables)
 value = computeValue(func, variables)
 
-print("RESULT =  " + str(value) + " pm " + str(error))
+print("RESULT = %.2f pm %.1f" % (value, error))
 
 
 
