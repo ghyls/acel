1a275e105691d651c12178eaea2b3fc83bdd7c7f
Mario <marioyales2@gmail.com>
2019-03-14 14:14:55 +0100

    updated, so it works fine with the new plotter

--- terminator.cpp
+++ terminator.cpp
@@ -18,94 +18,115 @@
 void terminator()
 {
 
-    MPI_Comm comm;
-    MPI_Status status;
-    int size, rank;
+  MPI_Comm comm;
+  MPI_Status status;
+  int size, rank;
   
-    comm = MPI_COMM_WORLD;
+  comm = MPI_COMM_WORLD;
   
-    MPI_Init(NULL, NULL);
+  MPI_Init(NULL, NULL);
   
-    MPI_Comm_size(comm, &size);
-    MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+  MPI_Comm_rank(comm, &rank);
   
-    std::cout << "Hello from rank " << rank << '!' << std::endl;
-    if (rank==0) {
-      std::cout << "Running on " << size << " process(es)!\n" << std::endl;}
+  std::cout << "Hello from rank " << rank << '!' << std::endl;
+  if (rank==0) {
+    std::cout << "Running on " << size << " process(es)!\n" << std::endl;}
 
-    std::string pathToFiles = "./practica/files/";
-    std::string dataFile = "data";
-    std::string prefix = "hello";    
+  std::string pathToFiles = "./practica/files/";
+  std::string dataFile = "data";
+  std::string prefix = "hello";    
 
-    std::vector<std::string> MCsamples;
+  std::vector<std::string> MCsamples;
 
 
-    std::vector<Int_t> colors = {kGray, kBlue-1, kTeal-1, kTeal+1, kTeal+4, \
-                                kAzure-8, kOrange+1, kRed+1};
+  std::vector<Int_t> colors = {kGray, kBlue-1, kTeal-1, kTeal+1, kTeal+4, \
+                              kAzure-8, kOrange+1, kRed+1};
 
-    MCsamples = {"qcd", "wjets", "ww", "wz", "zz", "dy", "single_top", "ttbar"};
-    static Plotter * plot;
+  MCsamples = {"qcd", "wjets", "ww", "wz", "zz", "dy", "single_top", "ttbar"};
+  static Plotter * plot;
 
-    int RANKS[3] = {0, 0, 0};
+  int RANKS[3] = {0, 0, 0};
+
+
+    plot = new Plotter(MCsamples, pathToFiles, "data");
+    std::vector<TString> histos;
+    //plot.SetColors(colors);
 
     if(rank==RANKS[0])
     {
-      // mu mu Pt >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-      plot = new Plotter(MCsamples, pathToFiles, "data");
-      //plot.SetColors(colors);
+      plot->SetLegendPos(0.7, 0.6, 0.9, 0.88);
+      plot->SetTitles("W Mass", "M_W", "Events", "leptWMass");
+      histos = {"MCMassHadrW", "MCMassLeptW"};
+      plot->Stack("", "ttbar", false, histos, false);
+
+      //plot->SetLegendPos(0.7, 0.6, 0.9, 0.88);
+      //plot->SetTitles("bTag eff", "uff", "Events", "bTagEff");
+      //histos = {"JetBTaggedRECO", "JetMatchedRECO"};
+      //plot->Stack("", "ttbar", false, histos, false);
 
       plot->SetTitles("Muon Pt", "muonPt", "Events", "muonPt");
-      plot->SetLegendPos(0.62, 0.6, 0.85, 0.85);
-      plot->Stack("MuonPt");
-  
+      plot->SetLegendPos(0.7, 0.6, 0.9, 0.88);
+      plot->Stack("MuonPt", "", true);
+
+      plot->SetTitles("tageados - matcheados", "jet is B", "Events", "bMatching");
+      plot->SetLegendPos(0.7, 0.6, 0.9, 0.88);
+      histos = {"JetMatchSuccess"};
+      plot->Stack("", "ttbar", false, histos, false);
+
       plot->SetTitles("MuMuMass", "MMMass", "Events", "muMuMass");
-      plot->SetLegendPos(0.6, 0.6, 0.9, 0.9);
-      plot->Stack("MuMuMass", true);
+      plot->SetLegendPos(0.7, 0.6, 0.9, 0.88);
+      plot->Stack("MuMuMass", "", true, {}, true);
 
       plot->SetTitles("Me gusta el miso", "MIso", "Events", "muon_Iso");
-      plot->SetLegendPos(0.6, 0.6, 0.9, 0.9);
-      plot->Stack("Muon_Iso", false, 20000);
+      plot->SetLegendPos(0.7, 0.6, 0.9, 0.88);
+      plot->Stack("Muon_Iso", "", true, {}, false, 20000);
+    }
 
 
-
+    if(rank==RANKS[1])
+    {
       plot->SetLegendPos(0.6, 0.75, 0.9, 0.9);
       plot->SetTitles("Eficiencia del trigger", "Ptmuon", "Events", "triggEff");
-      plot->plotWithRatio("ttbar", "MuonPt_TriggOnly", "MuonPt_raw", "RATIO", 0, 1.1);
+      plot->plotWithRatio("ttbar", "MuonPt_TriggOnly", "MuonPt_raw", "RATIO", 
+                          0, 1.1);
 
       plot->SetLegendPos(0.6, 0.75, 0.9, 0.9);
       plot->SetTitles("Aceptancia", "xda", "Events", "aceptancia");
-      plot->plotWithRatio("ttbar", "Acep_obs", "Acep_gen", "RATIO", 0, 1, false);
+      plot->plotWithRatio("ttbar", "Acep_obs", "Acep_gen", "RATIO", 0, 1, 
+                          false);
 
       plot->SetLegendPos(0.6, 0.75, 0.9, 0.9);
-      plot->SetTitles("BJet_Pt", "BJet pt", "Events", "deleteme");
-      plot->plotWithRatio("ttbar", "Hadr_BJet_Pt_GEN", "Lept_BJet_Pt_GEN", "RATIO", 0, 1, false);
+      plot->SetTitles("bTag eff", "uff", "Events", "bTagEff");
+      plot->plotWithRatio("ttbar", "JetBTaggedRECO", "JetMatchedRECO", 
+                          "RATIO", 0, 1, false);
       
       plot->SetLegendPos(0.6, 0.75, 0.9, 0.9);
-      plot->SetTitles("Bjets gen vs jets med.", "BJet pt", "Events", "eff B");
-      plot->plotWithRatio("ttbar", "BJet_Pt", "Jets_GEN_Pt", "RATIO", 0.3, 1.5, false);
-
-
+      plot->SetTitles("Bjets gen vs jets med.", "BJet pt", "Events", "effB");
+      plot->plotWithRatio("ttbar", "BJet_Pt", "Jets_GEN_Pt", "RATIO", 0.3, 1.5, 
+                          false);
 
       plot->PrintEvents("MuonPt");
       plot->PrintXSecData();
-      delete plot;
-      // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     }
-/*
+    delete plot;
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    
+
     if(rank==RANKS[1])
     {
     // Jet btag >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    plot = new Plotter({"wjets", "ttbar"}, pathToFiles, "");
-
-    plot->SetTitles("Jet btag","Jet_btag", "Events", "jet_btag");
-    plot->SetLegendPos(0.7, 0.8, 0.88, 0.88);
-    plot->Stack("Jet_btag", 0, 200);
-
-    plot->PrintEvents("Jet_btag");
-    delete plot;
+//    plot = new Plotter({"wjets", "ttbar"}, pathToFiles, "");
+//
+//    plot->SetTitles("Jet btag","Jet_btag", "Events", "jet_btag");
+//    plot->SetLegendPos(0.7, 0.8, 0.88, 0.88);
+//    plot->Stack("Jet_btag", "", false, 200);
+//
+//    plot->PrintEvents("Jet_btag");
+//    delete plot;
     // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     }
-*/
+
 
 
 
